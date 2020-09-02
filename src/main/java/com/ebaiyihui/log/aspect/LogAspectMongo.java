37f8783de78df1c1406b7e45cc09042b7f9f84ae
liyp <liyp@ebaiyihui.com>
2020-09-02 16:58:54 +0800

    优化

--- src/main/java/com/ebaiyihui/log/aspect/LogAspectMongo.java
+++ src/main/java/com/ebaiyihui/log/aspect/LogAspectMongo.java
@@ -13,6 +13,7 @@ import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
